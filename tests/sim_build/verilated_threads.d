c86d74bac911b139b994bc7353f683128123a8b0
M <emma.null256@gmail.com>
2025-08-30 20:20:35 +0200

    More tests!

--- tests/sim_build/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilated_threads.cpp \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilated_threads.h \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilated.h \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/emma/Documents/oss-cad-suite/share/verilator/include/verilated_funcs.h
